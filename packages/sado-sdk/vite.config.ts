c25198e18faff4cc1b996005fd602e91dd523e50
Kodemon <hi@kodemon.net>
2023-07-03 23:20:10 +0900

    chore(release): add vite library bundler

---
+++ packages/sado-sdk/vite.config.ts
@@ -0,0 +1,12 @@
+import { defineConfig } from "vite";
+import { resolve } from "node:path";
+
+export default defineConfig({
+  build: {
+    lib: {
+      entry: resolve(__dirname, "src/index.ts"),
+      name: "Sado",
+      fileName: "sado-sdk"
+    }
+  }
+});
